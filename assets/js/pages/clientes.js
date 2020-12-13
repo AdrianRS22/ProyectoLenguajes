9b5b6450ddab004db01dfb8a0cd54bf738aca38e
Adrian <adrianrojasanchez@gmail.com>
2020-12-13 17:39:44 -0600

    Añadiendo DataTables

---
+++ assets/js/pages/clientes.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $("#tablaClientes").DataTable({
+        searching: false,
+        bInfo: false,
+        paging: false,
+        orden: []
+    });
+
+});
--- cliente.php
+++ cliente.php
@@ -4,10 +4,25 @@ require_once 'includes/cabecera.php';
 ?>
 
 <div class="container" id="contenido">
-    
-    
+    <a href="#" class="btn btn-lg btn-primary mb-3">Añadir Cliente</a>
+
+    <table id="tablaClientes" class="table" style="width:100%">
+        <thead class="thead-dark">
+            <tr>
+                <th>Id</th>
+                <th>Nombre Completo</th>
+                <th>Dirección</th>
+                <th>Correo</th>
+                <th>Teléfono</th>
+            </tr>
+        </thead>
+    </table>
 </div>
+
+
+
 <?php require_once 'includes/footer.php' ?>
+<script src="assets/js/pages/clientes.js"></script>
 </body>
 
 </html>
