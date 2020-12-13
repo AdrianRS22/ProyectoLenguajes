2c129d67e9b4266c014fa891d9378910c736cb6f
Adrian <adrianrojasanchez@gmail.com>
2020-12-13 17:20:52 -0600

    Agregando portada

--- cliente.php
+++ cliente.php
@@ -4,7 +4,7 @@ require_once 'includes/cabecera.php';
 ?>
 
 <div class="container" id="contenido">
-
+    
     
 </div>
 <?php require_once 'includes/footer.php' ?>
--- index.php
+++ index.php
@@ -3,9 +3,31 @@ require_once 'includes/cabecera.php';
 
 ?>
 
-<div class="container" id="contenido">
+<div class="container" id="contenido" class="text-center">
 
+    <div class="text-center">
+        <h1>Proyecto Final Lenguajes de Base de Datos</h1>
+        <hr>
 
+        <img src="assets/img/fidelitas.png" class="img-fluid mb-3" alt="Fidelitas Logo">
+        <div class="clearfix"></div>
+        
+        <strong>Estudiantes </strong>
+        <ul>
+            <li>Adrián Rojas Sánchez</li>
+            <li>Estiven Oviedo Alfaro</li>
+        </ul>
+
+        <strong>Profesor</strong>
+        <ul>
+            <li>Clarence Ricketts Torres</li>
+        </ul>
+
+        <strong>Horario</strong>
+        <p>Martes 6pm - 9pm</p>
+
+        <strong>III Cuatrimestre 2020</strong>
+    </div>
 </div>
 
 <?php require_once 'includes/footer.php' ?>
